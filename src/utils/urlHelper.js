430a20dc099a47c93a3428df1498f8a61c907f27
muhammadafiqyunanto27 <muhammadafiqyunanto@gmail.com>
2026-04-15 20:22:25 +0700

    fix swipe to reply

--- src/utils/urlHelper.js
+++ src/utils/urlHelper.js
@@ -13,9 +13,10 @@ const getAbsoluteUrl = (pathOrUrl) => {
   
   if (process.env.RAILWAY_PUBLIC_DOMAIN) {
     baseUrl = `https://${process.env.RAILWAY_PUBLIC_DOMAIN}`;
-  } else if (process.env.BACKEND_URL) {
+  } else if (process.env.NODE_ENV === 'production' && process.env.BACKEND_URL) {
     baseUrl = process.env.BACKEND_URL;
   }
+  // If local dev, baseUrl remains http://localhost:5000 regardless of BACKEND_URL in .env
   
   // Ensure we don't double slash
   const cleanPath = pathOrUrl.startsWith('/') ? pathOrUrl : `/${pathOrUrl}`;
